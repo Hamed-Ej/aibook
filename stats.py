ff77fce722f53159c9b74ba4940205dcaf1b50b7
hamed <hamedeg125@gmail.com>
2026-01-30 15:01:51 +0330

    adding stat in python

--- stats.py
+++ stats.py
@@ -1,1 +1,23 @@
-# just a place holder
+# writing a python script to calculate the average of a list of numbers
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+print(average(numbers))
+
+# writing a python script to calculate the median of a list of numbers
+
+def median(numbers):
+    return sorted(numbers)[len(numbers) // 2]
+
+numbers = [1, 2, 3, 4, 5]
+print(median(numbers))
+
+# writing a python script to calculate the mode of a list of numbers
+
+def mode(numbers):
+    return max(set(numbers), key=numbers.count)
+
+numbers = [1, 2, 3, 4, 5]
+print(mode(numbers))
